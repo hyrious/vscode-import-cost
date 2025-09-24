8e929ba43b7150f223af9358584be424fb2b25d7
hyrious <hyrious@outlook.com>
2025-09-24 16:23:14 +0800

    support vscode ^1.99

--- src/build.ts
+++ src/build.ts
@@ -5,10 +5,10 @@ await build({
   entryPoints: ['src/extension.ts'],
   bundle: true,
   platform: 'node',
-  format: 'esm',
+  format: 'cjs', // TODO: change to 'esm' once Cursor uses VS Code 1.100+
   mainFields: ['module', 'main'],
   external: ['vscode', 'esbuild'],
-  plugins: [external()],
+  // plugins: [external()],
   outdir: 'out',
   sourcemap: true,
   sourcesContent: false,
